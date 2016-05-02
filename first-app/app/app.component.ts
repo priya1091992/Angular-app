6a57fdb13a8fac25464ad1702c82165a8b3a886e
Priya Choudhary <priya@tothenew.com>
2016-05-02 22:28:25 +0530

    First application in angular2.0

---
+++ first-app/app/app.component.ts
@@ -0,0 +1,33 @@
+import {Component} from 'angular2/core';
+import {Hero} from 'app/app.hero';
+import {HeroDetailsComponent} from 'app/app.hero-details';
+
+@Component({
+  selector: 'hero-app',
+  templateUrl:'app/app.component.html',
+  directives:[HeroDetailsComponent]
+})
+
+export class AppComponent {
+  title='First-App';
+  heroes=HEROES;
+  selectedHero;
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+
+}
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
+
+
+
